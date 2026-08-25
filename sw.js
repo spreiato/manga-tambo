8625039927f4154979a2413d0f2d435b9f53f77a
spreiato <santiago.m.preiato@gmail.com>
2026-08-25 13:50:12 -0300

    Version1

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'manga-tambo-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
