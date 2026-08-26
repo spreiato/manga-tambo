af56617380bd70832edef5eaa402b89d5cf6ef09
spreiato <santiago.m.preiato@gmail.com>
2026-08-26 20:13:14 -0300

    ventas

---
+++ sw_ventas.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'ventas-tambo-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') {
+    return;
+  }
+
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
