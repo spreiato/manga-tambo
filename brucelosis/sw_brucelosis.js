f5c3407218b1bda31f637009c0b2afc3edb2e89d
spreiato <santiago.m.preiato@gmail.com>
2026-08-27 20:34:50 -0300

    Add service worker for caching in brucelosis

---
+++ brucelosis/sw_brucelosis.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'brucelosis-v1';
+const urlsToCache = [
+  './brucelosis.html',
+  './manifest_brucelosis.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
