feee1f68a4ef868644a7f6957fa109235d868205
spreiato <santiago.m.preiato@gmail.com>
2026-08-27 20:52:06 -0300

    Add service worker for caching assets

---
+++ fc_vacias/sw_fc_vacias.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'fc-vacias-v1';
+const urlsToCache = [
+  './fc_vacias.html',
+  './manifest_fc_vacias.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
