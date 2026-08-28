fc6ff021ce7e348defe6052f3a1f113a4adee755
spreiato <santiago.m.preiato@gmail.com>
2026-08-27 21:42:11 -0300

    Implement service worker for caching assets

---
+++ fc_llenas/sw_fc_llenas.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'fc-llenas-v1';
+const ASSETS = [
+  './',
+  './index_fc_llenas.html',
+  './manifest_fc_llenas.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    }).catch(() => {
+      return caches.match('./index_fc_llenas.html');
+    })
+  );
+});
