84a4263ff21c9c3865d0231f10802665f46bb734
spreiato <santiago.m.preiato@gmail.com>
2026-08-27 22:00:02 -0300

    Create sw_inseminacion.js

---
+++ inseminacion/sw_inseminacion.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'inseminacion-v1';
+const BASE_PATH = self.location.pathname.substring(0, self.location.pathname.lastIndexOf('/') + 1);
+
+const ASSETS = [
+  BASE_PATH,
+  BASE_PATH + 'index_inseminacion.html',
+  BASE_PATH + 'manifest_inseminacion.json'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200) {
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return networkResponse;
+      });
+    }).catch(() => {
+      return caches.match(BASE_PATH + 'index_inseminacion.html') || caches.match(BASE_PATH);
+    })
+  );
+});
